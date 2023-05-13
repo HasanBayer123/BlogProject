6c9602057898540e82e744cbe799462ac049ceac
Hasan Bayer <95939003+HasanBayer@users.noreply.github.com>
2023-05-13 22:28:12 +0300

    Add files via upload

---
+++ CoreBlogAPI/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreBlogAPI.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LCK25ST;database=CoreBlogApiDb; integrated security=true; TrustServerCertificate=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
